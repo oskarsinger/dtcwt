ca8f82334440438b43337db4f181b3d7777a2011
oskar <oskarsinger@gmail.com>
2017-10-25 17:54:22 -0400

    Started e4 wavelet generator

---
+++ run_generate_e4_wavelets.py
@@ -0,0 +1,61 @@
+import click
+import os
+
+from dtcwt.mask import DTCWTMask as DTCWTM
+from whitehorses.servers.masks import Interp1DMask as I1DM
+from whitehorses.servers.batch import BatchServer as BS
+from drrobert.file_io import get_timestamped as get_ts
+
+import whitehorses.loaders.shortcuts as dlsh
+
+@click.command()
+@click.option('--data-path')
+@click.option('--wavelet-dir')
+@click.option('--save-dir', default='.')
+@click.option('--num-subperiods', default=288)
+@click.option('--interpolate', default=False)
+@click.option('--max-freqs', default=7)
+@click.option('--max-hertz', default=4)
+@click.option('--pr', default=False)
+@click.option('--overlap', default=True)
+@click.option('--csv', default=True)
+def run_it_all_day_bb(
+    data_path,
+    wavelet_dir,
+    save_dir,
+    num_subperiods,
+    interpolate,
+    max_freqs,
+    max_hertz,
+    pr,
+    overlap,
+    csv):
+
+    loaders = dlsh.get_hr_and_acc_all_subjects(
+        data_path, max_hertz=max_hertz)
+    servers = {s : [BS(dl) for dl in dls]
+               for (s, dls) in loaders.items()}
+    wavelet_dir = os.path.join(
+        wavelet_dir, get_ts('DTCWT'))
+
+    os.mkdir(wavelet_dir) 
+
+    if interpolate:
+        servers = {s : [I1DM(ds) for ds in dss]
+                   for (s, dss) in servers.items()}
+
+    servers = {s : [DTCWTM(
+                        ds, 
+                        wavelet_dir, 
+                        magnitude=True,
+                        pr=pr,
+                        period=int(24*3600 / num_subperiods),
+                        max_freqs=max_freqs,
+                        overlap=overlap,
+                        save=save_dir,
+                        csv=csv)
+                    for ds in dss]
+               for (s, dss) in servers.items()}
+
+if __name__=='__main__':
+    run_it_all_day_bb()
